9edc29036070f4d11842a2eef7628fffe259d1a9
Tomas Germanavicius <tomas@metrika.co>
2022-01-21 14:38:10 +0000

    Tsts on prometheus pef parsing.

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/client_model v0.2.0
-	github.com/prometheus/common v0.26.0
+	github.com/prometheus/common v0.32.1
 	github.com/prometheus/procfs v0.7.3
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
@@ -25,7 +25,7 @@ require (
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
 	google.golang.org/protobuf v1.26.0-rc.1 // indirect
 )
--- pkg/parse/openmetrics/pef_test.go
+++ pkg/parse/openmetrics/pef_test.go
@@ -1,12 +1,16 @@
 package openmetrics
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"math"
 	"testing"
 
 	"agent/api/v1/model"
 
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -297,5 +301,134 @@ func BenchmarkParsePEF(t *testing.B) {
 	for i := 0; i < t.N; i++ {
 		_, err := ParsePEF(data, nil)
 		require.NoError(t, err)
+	}
+}
+
+func BenchmarkPefDecoder(bech *testing.B) {
+	data, err := ioutil.ReadFile(ValidTests + "pef_full")
+	require.NoError(bech, err, "failed to read test file")
+
+	for i := 0; i < bech.N; i++ {
+		buf := bytes.NewBuffer(data)
+		dec := expfmt.NewDecoder(buf, expfmt.FmtText)
+		for {
+			mf := dto.MetricFamily{}
+			if err := dec.Decode(&mf); err != nil && err == io.EOF {
+				break
+			} else if err != nil {
+				bech.Fatalf("received error while decoding PEF: %v", err)
+			}
+			// fmt.Printf("%+v", mf)
+			// bech.Logf("%+v\n\n", mf)
+		}
+	}
+}
+
+func BenchmarkExpfmtText(bech *testing.B) {
+	data, err := ioutil.ReadFile(ValidTests + "pef_full")
+	require.NoError(bech, err, "failed to read test file")
+	var parser expfmt.TextParser
+	for i := 0; i < bech.N; i++ {
+		buf := bytes.NewBuffer(data)
+		parser.TextToMetricFamilies(buf)
+	}
+}
+
+func TestPefDecoder(t *testing.T) {
+	b := readFileOrFail(t, ValidTests+"pef_full")
+
+	buf := bytes.NewBuffer(b)
+	dec := expfmt.NewDecoder(buf, expfmt.FmtText)
+	for {
+		mf := dto.MetricFamily{}
+		if err := dec.Decode(&mf); err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatalf("received error while decoding PEF: %v", err)
+		}
+		// fmt.Printf("%+v", mf)
+		t.Logf("%+v\n\n", mf)
+	}
+}
+
+func TestExpfmtText(t *testing.T) {
+	b := readFileOrFail(t, ValidTests+"pef_full")
+
+	buf := bytes.NewBuffer(b)
+	parser := expfmt.TextParser{}
+	mfs, err := parser.TextToMetricFamilies(buf)
+	require.NoError(t, err)
+	t.Log(mfs)
+}
+
+func TestParsePEF_Failures2(t *testing.T) {
+	cases := []struct {
+		Description string
+		Data        []byte
+	}{
+		// {"Incomplete file", readFileOrFail(t, FailTests+"pef_incomplete")},
+		{"Missing histogram params", readFileOrFail(t, FailTests+"pef_histogram1")},
+		// {"Missing summary params", readFileOrFail(t, FailTests+"pef_summary1")},
+		{"Invalid histogram params", readFileOrFail(t, FailTests+"pef_histogram2")},
+		{"Invalid summary params", readFileOrFail(t, FailTests+"pef_summary2")},
+		{"Missing label closure",
+			[]byte("http_requests_total{method=\"post\",code=\"200\" 1027 1395066363000\n")},
+		{"Missing label value closure",
+			[]byte("http_requests_total{method=\"post\",code=\"200} 1027 1395066363000\n")},
+		{"Unescaped label sequence", []byte("http_requests_total{method=\"p\"ost\",code=\"200\"} 1027 1395066363000\n")},
+
+		{"Invalid value type", []byte("metric_without_timestamp_and_labels asdf\n")},
+		{"Invalid timestamp type", []byte("http_requests_total 1027 139506.12\n")},
+		{"Invalid help line", []byte("# HELP metric_name\n")},
+		{"Invalid type line", []byte("# TYPE metric_name type1 and some extra info\n")},
+	}
+
+	for _, testcase := range cases {
+		buf := bytes.NewBuffer(testcase.Data)
+		dec := expfmt.NewDecoder(buf, expfmt.FmtOpenMetrics)
+		for {
+			mf := dto.MetricFamily{}
+			if err := dec.Decode(&mf); err != nil && err == io.EOF {
+				t.Fatalf("Expected error when running TestParsePEF['%s']", testcase.Description)
+			} else if err != nil {
+				t.Logf("Got error we wanted: %v\n", err)
+				break
+			}
+			// fmt.Printf("%+v", mf)
+			// t.Logf("%+v\n\n", mf)
+		}
+	}
+}
+
+func TestParsePEF_Failures3(t *testing.T) {
+	cases := []struct {
+		Description string
+		Data        []byte
+	}{
+		{"Incomplete file", readFileOrFail(t, FailTests+"pef_incomplete")},
+		{"Missing histogram params", readFileOrFail(t, FailTests+"pef_histogram1")},
+		{"Missing summary params", readFileOrFail(t, FailTests+"pef_summary1")},
+		{"Invalid histogram params", readFileOrFail(t, FailTests+"pef_histogram2")},
+		{"Invalid summary params", readFileOrFail(t, FailTests+"pef_summary2")},
+		{"Missing label closure",
+			[]byte("http_requests_total{method=\"post\",code=\"200\" 1027 1395066363000\n")},
+		{"Missing label value closure",
+			[]byte("http_requests_total{method=\"post\",code=\"200} 1027 1395066363000\n")},
+		{"Unescaped label sequence", []byte("http_requests_total{method=\"p\"ost\",code=\"200\"} 1027 1395066363000\n")},
+
+		{"Invalid value type", []byte("metric_without_timestamp_and_labels asdf\n")},
+		{"Invalid timestamp type", []byte("http_requests_total 1027 139506.12\n")},
+		{"Invalid help line", []byte("# HELP metric_name\n")},
+		{"Invalid type line", []byte("# TYPE metric_name type1 and some extra info\n")},
+	}
+
+	var parser expfmt.TextParser
+	for _, testcase := range cases {
+		buf := bytes.NewBuffer(testcase.Data)
+		_, err := parser.TextToMetricFamilies(buf)
+		if err != nil {
+			t.Logf("Good! got: %v\n", err)
+		}
+		assert.Error(t, err, "Case ['%s']", testcase.Description)
 	}
 }
