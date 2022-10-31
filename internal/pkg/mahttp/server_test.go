1a1f419dd90f28df079516be73ac00dcf56bc7e3
Ioannis Gakos <igakos@metrika.co>
2022-10-31 22:33:43 +0200

    Common HTTP handler for local endpoint and Host header validation
    Signed-off-by: Ioannis Gakos <igakos@metrika.co>

---
+++ internal/pkg/mahttp/server_test.go
@@ -0,0 +1,79 @@
+package mahttp
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+
+	"agent/internal/pkg/global"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/stretchr/testify/require"
+)
+
+// TestStartHTTPServer_HostHeaderValidation tests that HTTP servers
+// started by StartHTTPServer() and paths wrapped with ValidationMiddleware(),
+// validate host header and reject requests for which validation fails.
+func TestStartHTTPServer_HostHeaderValidation(t *testing.T) {
+	tests := []struct {
+		name         string
+		addr         string
+		endpoint     string
+		hostHeader   string
+		allowedHosts []string
+		expCode      int
+	}{
+		{
+			name:         "valid",
+			addr:         "127.0.0.1:9001",
+			endpoint:     "/metrics",
+			allowedHosts: []string{"127.0.0.1"},
+			hostHeader:   "127.0.0.1",
+			expCode:      200,
+		},
+		{
+			name:         "invalid host header",
+			addr:         "127.0.0.1:9001",
+			endpoint:     "/metrics",
+			allowedHosts: []string{"127.0.0.1"},
+			hostHeader:   "foobar",
+			expCode:      400,
+		},
+	}
+
+	promHandler := promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{EnableOpenMetrics: true})
+	http.Handle("/metrics", ValidationMiddleware(promHandler))
+
+	allowedHostsWas := global.AgentConf.Runtime.AllowedHosts
+	defer func() { global.AgentConf.Runtime.AllowedHosts = allowedHostsWas }()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			global.AgentConf.Runtime.AllowedHosts = tt.allowedHosts
+
+			wg := &sync.WaitGroup{}
+			wg.Add(1)
+			srv := StartHTTPServer(wg, tt.addr)
+			time.Sleep(1 * time.Second)
+
+			req, err := http.NewRequest("GET", "http://127.0.0.1:9001/metrics", nil)
+			require.Nil(t, err)
+			req.Host = tt.hostHeader
+			resp, err := http.DefaultClient.Do(req)
+			require.NoError(t, err)
+
+			require.Equal(t, tt.expCode, resp.StatusCode)
+
+			httpctx, httpcancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer httpcancel()
+
+			err = srv.Shutdown(httpctx)
+			require.Nil(t, err)
+
+			wg.Wait()
+		})
+	}
+}
